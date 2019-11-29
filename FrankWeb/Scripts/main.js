cfa19f8b1b081fd63b2e41902a9aedf24e471865
CryptoSynth <lramirez.army@gmail.com>
2019-11-29 14:48:03 -0500

    VS Code

---
+++ FrankWeb/Scripts/main.js
@@ -0,0 +1,10 @@
+﻿var app = new Vue({
+    el: '#app',
+    data: {
+        product: 'Socks',
+        image: './Images/test1.jpg',
+        source: 'https://www.youtube.com/',
+        inStock: true,
+        details: ["80% cooton","20% polyester","Gender-neutral"]
+    }
+})
