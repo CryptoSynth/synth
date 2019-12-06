0a1f436aa0c31299222842d621e795a7f0cb138b
CryptoSynth <lramirez.army@gmail.com>
2019-12-05 20:20:35 -0500

    changes to loader for buttons, added censor stripe, and fixed the responsiveness for the video on chronos 1.4 page

--- TestBootStrap/Scripts/buttonLoader.js
+++ TestBootStrap/Scripts/buttonLoader.js
@@ -1,5 +1,6 @@
-$(document).ready(function() {
-    $("#btnLoader").click(function() {
+var elements = document.querySelectorAll("#btnLoader");
+for (var i = 0; i < elements.length; i++) {
+    $(elements[i]).click(function() {
         // disable button
         $(this).prop("disabled", true);
         // add spinner to button
@@ -7,4 +8,4 @@ $(document).ready(function() {
             `<span class="spinner-border spinner-style" role="status" aria-hidden="true"></span>`
         );
     });
-});
+}
