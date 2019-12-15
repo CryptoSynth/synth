f6035defb154459dc472f55ff2e9cacf4cef82a3
CryptoSynth <lramirez.army@gmail.com>
2019-12-15 18:31:36 -0500

    homepage tweaked, store fixed

--- TestBootStrap/Scripts/SideMenu.js
+++ TestBootStrap/Scripts/SideMenu.js
@@ -2,16 +2,23 @@ $(document).ready(function() {
     $("#dismiss, .overlay").on("click", function() {
         // hide sidebar
         $("#sidebar").removeClass("active");
+
         // hide overlay
         $(".overlay").removeClass("active");
+
+        //
+
     });
 
-    $("#sidebarCollapse").on("click", function() {
+    var $hamburger = $(".hamburger");
+    $hamburger.on("click", function(e) {
+        $hamburger.toggleClass("is-active");
+
         // open sidebar
         $("#sidebar").addClass("active");
+
         // fade in the overlay
         $(".overlay").addClass("active");
         $(".collapse.in").toggleClass("in");
     });
-
 });
