14f5257fdc197a4dfa4d63ec9503409aad7d0a95
CryptoSynth <lramirez.army@gmail.com>
2019-12-23 17:39:23 -0500

    product pages, home, about us

--- TestBootStrap/Scripts/SideMenu.js
+++ TestBootStrap/Scripts/SideMenu.js
@@ -20,4 +20,60 @@ $(document).ready(function() {
         $(".overlay").addClass("active");
         $(".collapse.in").toggleClass("in");
     });
+
+    //on hover display text in overlay
+    $("#home").mouseover(function() {
+        $("#title-hover").text($("#home-title").text());
+    });
+    $("#home").mouseout(function() {
+        $("#title-hover").text("");
+    });
+    $("#about-us").mouseover(function() {
+        $("#title-hover").text($("#aboutus-title").text());
+    });
+    $("#about-us").mouseout(function() {
+        $("#title-hover").text("");
+    });
+    $("#chronos-1").mouseover(function() {
+        $("#title-hover").text($("#chronos-1-title").text());
+    });
+    $("#chronos-1").mouseout(function() {
+        $("#title-hover").text("");
+    });
+    $("#chronos-2").mouseover(function() {
+        $("#title-hover").text($("#chronos-2-title").text());
+    });
+    $("#chronos-2").mouseout(function() {
+        $("#title-hover").text("");
+    });
+    $("#accessories").mouseover(function() {
+        $("#title-hover").text($("#accessories-title").text());
+    });
+    $("#accessories").mouseout(function() {
+        $("#title-hover").text("");
+    });
+    $("#tutorials").mouseover(function() {
+        $("#title-hover").text($("#tutorials-title").text());
+    });
+    $("#tutorials").mouseout(function() {
+        $("#title-hover").text("");
+    });
+    $("#downloads").mouseover(function() {
+        $("#title-hover").text($("#downloads-title").text());
+    });
+    $("#downloads").mouseout(function() {
+        $("#title-hover").text("");
+    });
+    $("#resources").mouseover(function() {
+        $("#title-hover").text($("#resources-title").text());
+    });
+    $("#resources").mouseout(function() {
+        $("#title-hover").text("");
+    });
+    $("#contact-us").mouseover(function() {
+        $("#title-hover").text($("#contact-us-title").text());
+    });
+    $("#contact-us").mouseout(function() {
+        $("#title-hover").text("");
+    });
 });
