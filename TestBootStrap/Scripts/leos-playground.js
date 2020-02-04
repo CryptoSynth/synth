5e0b3ddf1e30c10d338b5163dbd86c66439f078e
CryptoSynth <lramirez.army@gmail.com>
2020-02-03 19:16:03 -0500

    card

--- TestBootStrap/Scripts/leos-playground.js
+++ TestBootStrap/Scripts/leos-playground.js
@@ -3,15 +3,46 @@ $(document).ready(function() {
     Input Here
 =================================================================*/
 
+    $(".img-overlay").on('click', function() {
+        $(this).fadeTo(500, 0, function() {
+            $(this).css('visibility', 'hidden');
+        });
+    });
+
     $(".carousel-body").slick({
         arrows: false,
         infinite: true,
+        speed: 400,
         slidesToShow: 3,
         centerMode: true,
-        centerPadding: "60px",
+        centerPadding: "100px",
         focusOnSelect: true,
         useTransform: true,
-        zIndex: 1000
+        responsive: [{
+                breakpoint: 425,
+                settings: {
+                    slidesToShow: 1,
+                    centerMode: true,
+                    centerPadding: "10px"
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                    centerMode: true,
+                    centerPadding: "10px"
+                }
+            },
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    centerMode: true,
+                    centerPadding: "0px"
+                }
+            }
+        ]
     });
 
 });
